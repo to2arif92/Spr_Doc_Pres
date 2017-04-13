0f88624eeb699b883b8a9a2b7f0e2f4c60e56a2c
ArIF <to2arif92@gmail.com>
2017-04-13 14:57:53 +0600

    + Login/Reg page with resource mapping (now default controller)

--- src/main/java/pkg/spring/basic/config/WebMvcConfig.java
+++ src/main/java/pkg/spring/basic/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package pkg.spring.basic.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -11,6 +12,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter{
+
+    // Static resource path config
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("/fonts/");
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/resources/js/");
+    }
+
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
--- src/main/java/pkg/spring/basic/controller/DefaultController.java
+++ src/main/java/pkg/spring/basic/controller/DefaultController.java
@@ -1,11 +1,18 @@
 package pkg.spring.basic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by ArIF on 12-Apr-17.
  */
 @Controller
 public class DefaultController {
-
+    @RequestMapping("/*")
+    public String index(Model model)
+    {
+        System.out.println("- method invoked: DefaultController");
+        return "index";
+    }
 }
